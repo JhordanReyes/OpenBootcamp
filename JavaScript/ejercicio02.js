a43ebaefac600907c7ed973cd08e1863c32e1127
Jhordan Reyes <reyesjhordan4@gmail.com>
2022-06-19 17:29:42 -0500

    Se agrego el segundo ejercicio de JavaScript

---
+++ JavaScript/ejercicio02.js
@@ -0,0 +1,22 @@
+const fechaNacimiento = new Date("june 04 2004");
+
+const persona = {
+    nombre: "Jhordan",
+    edad: 18,
+    desarrollador: false,
+    fechaNacimiento,
+    libros: [
+        {
+            titulo: "It",
+            author: "Stephen King",
+            fecha: "1990",
+            url: "https://www.casadellibro.com/libro-it/9788497593793/873687"
+        },
+        {
+            titulo: "AGUJEROS NEGROS",
+            author: "STEPHEN HAWKING",
+            fecha: "1971",
+            url: "https://www.casadellibro.com/libro-agujeros-negros/9788416771578/4819357"
+        }
+    ]
+}
