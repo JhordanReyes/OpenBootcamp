adff6b16e9475d97436f0e0a6ac4082ec7d41762
JhordanReyes <95984454+JhordanReyes@users.noreply.github.com>
2022-07-13 16:12:31 -0500

    Nuevo curso

---
+++ Java/ejercicio01.java
@@ -0,0 +1,33 @@
+package Java;
+
+/**
+ * @author Jhordan
+ */
+
+public class ejercicio01 {
+    public static void main(String[] args) {
+        
+        // NUMEROS
+        byte var1 = 1;
+        short var2 = 10;
+        int var3 = 234;
+        long var4 = 123;
+        // DECIMALES
+        float var5 = 23.4f;
+        double var6 = 23.2;
+        // BOLEANOS
+        boolean var7 = false;
+        boolean var8 = true;
+        // TEXTO
+        String var10 = "Hello World";
+        
+        System.out.println("NUMEROS ------------------");
+        System.out.println("byte: " + var1 + "\nshort: " + var2 + "\nint: " + var3 + "\nlong: " + var4 + "\n");
+        System.out.println("DECIMALES ----------------");
+        System.out.println("float: " + var5 + "\ndouble: " + var6 + "\n");
+        System.out.println("BOOLEANOS ----------------");
+        System.out.println("boolean: " + var7 + "\nboolean: " + var8 + "\n");
+        System.out.println("TEXTO --------------------");
+        System.out.println("String: " + var10);
+    }    
+}
